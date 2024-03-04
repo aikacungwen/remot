23f9e21ad4d4237c32d47f4cf37c50b62d49efaf
aika cungwen <70378095+aikacungwen@users.noreply.github.com>
2024-03-04 13:06:42 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Remote-File-Upload
+Upload FIle to server with percentage progress
+
+<img src="https://raw.githubusercontent.com/Pedroxam/Remote-File-Upload/master/shot.jpg">
+
+This is easy way to remote upload your file to target server.
+
+1 - First upload all files to your host. then open index.php and replace your server url.(this file have comment). 
+
+2 - Second, upload remote.php on your target server.
+
+3 - Run Script and enjoy!
+
+Thanks
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * By Pedram [ Pedroxam ]
+*/
+
+// Replace your server url and remote.php file
+define('SERVER_API','http://example.com/remote.php');
+?>
+
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title>Remote Upload File</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+	<script src="./jquery.min.js"></script>
+</head>
+<body>
+	<div id="upload">
+		<h2>Upload Now !</h2>
+		<form id="remote">
+			<fieldset>
+				<p><label for="link">Link</label></p>
+				<p><input type="url" id="url" name="url" placeholder="http://example.com/file.zip" required autocomplete="off"></p>
+				<p><label for="link">Filename</label></p>
+				<p><input type="text" id="name" name="name" placeholder="file.zip" required autocomplete="off"></p>
+				<p><input type="submit" value="Upload" name="send" class="Button"></p>
+			</fieldset>
+		</form>
+		<div id="result">
+			Progress: <span class="progress">--</span>
+			<br/>
+			<br/>
+			Uploaded: <span class="uploaded">--</span>
+			<br/>
+			<br/>
+			Filesize: <span class="filesize">--</span>
+		</div>
+	</div>
+	
+<script>
+
+	// Global  Progress var
+	var doProgress;
+		
+	function getProgress(){
+		$.ajax({
+			url: '<?php echo SERVER_API; ?>?progress=1',
+			dataType: 'json',
+			type: 'POST',
+			data: {
+				name: $('#name').val()
+			},
+			success:function(data){
+				$('#result .progress').html(data.progress + ' %');
+				$('#result .uploaded').html(data.uploaded);
+				$('#result .filesize').html(data.size);
+				
+				if(data.progress >= 100){
+				    clearInterval(doProgress); // Stop  Progress Var
+				    $('#result').html('File Succesfully Uploaded!');
+				}
+			},
+		})
+	}
+
+	$(document).ready(function(){
+		$('#remote').submit(function(e){
+			
+		  	e.preventDefault();
+			
+			if($('#url').val() === '') return;
+			if($('#name').val() === '') return;
+			
+			$.ajax({
+				url: '<?php echo SERVER_API; ?>',
+				dataType: 'json',
+				type: 'POST',
+				data: $(this).serialize(),
+				beforeSend:function()
+				{
+					$('.Button').attr('disabled',true);
+					doProgress = setInterval(getProgress, 2500);
+				},
+				success:function(data){
+					$('.Button').attr('disabled',false);
+					
+					if(data.status){
+						$('#result').html('File Succesfully Uploaded.');
+					}
+					else {
+						$('#result').html('Sorry, Please Check your server config.');
+					}
+				},
+			})
+		});
+	});
+	
+</script>
+
+</body>
+</html>
